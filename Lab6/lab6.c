e9813b31ed42481507cb3781e60f7e894c2bf8d5
wogy14 <wogy.wogy14@gmail.com>
2018-10-16 16:04:32 -0400

    update lab6.c

--- Lab6/lab6.c
+++ Lab6/lab6.c
@@ -1,51 +1,48 @@
 #include <stdio.h>
 #include <ctype.h>
-#include <cs50.h>
-int is_letter(char c){
-    if((c >= 65 && c <= 90) || (c >= 97 && c <= 122)){
-        return 1;
-    }
-    return 0;
+#include <string.h>
+#define DEFAULT_LEN 255
+
+int is_loud(char c) {
+	int arr[6] = { 'a','e','i','o','u','y' }, i = 0;
+	c = tolower(c);
+	for (i = 0; i < 6; i++) {
+		if (c == arr[i]) {
+			return 1;
+		}
+	}
+	return 0;
 }
-int is_loud(char c){    
-    int arr[6] = {97,101,105,111,117,121},i = 0;      
-    c = tolower(c);
-    for(i = 0; i < 6;i++){
-        if(c == arr[i]){
-            return 1;
-        }
-    }
-    return 0;
-}
-int main(){
-    char *text;    
-    int i = 0,start_w,sw,count = 0;   
-    printf("Put the string: ");
-    text = GetString();   
-    while(text[i] != 46){
-        if(is_letter(text[i])){                    
-            start_w = i;
-            sw = 1;                       
-            while(text[i] != 32 && text[i] != 46){   
-                if(is_loud(text[i])){                    
-                    sw = 0;                                      
-                }  
-                i++;               
-            }            
-            if(sw == 1){
-                count++;
-                printf("%d. ",count);
-                while(text[start_w] != 32 && text[start_w] != 46){
-                    printf("%c",text[start_w]);
-                    start_w++;
-                }
-                printf("\n");
-            }
-        }
-        else{
-            i++;
-        }       
-    }
-    
-    return 0;
+int main() {
+	char text[DEFAULT_LEN];
+	int i = 0,count = 0;
+	printf("Put the string: ");
+	fgets(text, DEFAULT_LEN, stdin);	
+	while (text[i] != '\0') {
+		if (text[i] == ' ' || text[i] == '.' || text[i] == '\n') {
+			text[i] = '\0';
+		}		
+		i++;
+	}
+	for (i = 0; i < DEFAULT_LEN;i++) {
+		int checker = 1,start = i;
+		while (text[i] != '\0') {		    
+			if (!isalpha(text[i])) {
+				checker = 0;
+			}
+			if (is_loud(text[i])) {
+				checker = 0;
+			}
+			i++;
+		}
+		if (checker && isalpha(text[start])) {			
+			printf("%i. ",++count);
+			while (text[start] != '\0') {
+				printf("%c",text[start]);
+				start++;
+			}
+			printf("\n");
+		}
+	}
+	return 0;
 }
