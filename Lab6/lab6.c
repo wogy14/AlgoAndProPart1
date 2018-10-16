91a1e8a816c2e91db83d8cea5558fe2f02c286b3
wogy14 <wogy.wogy14@gmail.com>
2018-10-16 16:19:36 -0400

    update lab6.c

--- Lab6/lab6.c
+++ Lab6/lab6.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <ctype.h>
-#include <string.h>
 #define DEFAULT_LEN 255
 
 int is_loud(char c) {
 	int arr[6] = { 'a','e','i','o','u','y' }, i = 0;
 	c = tolower(c);
-	for (i = 0; i < 6; i++) {
-		if (c == arr[i]) {
-			return 1;
-		}
-	}
+	for (i = 0; i < 6; i++) if (c == arr[i]) return 1;
 	return 0;
 }
 int main() {
@@ -19,21 +14,14 @@ int main() {
 	printf("Put the string: ");
 	fgets(text, DEFAULT_LEN, stdin);	
 	while (text[i] != '\0') {
-		if (text[i] == ' ' || text[i] == '.' || text[i] == '\n') {
-			text[i] = '\0';
-		}		
+		if (text[i] == ' ' || text[i] == '.' || text[i] == '\n') text[i] = '\0';				
 		i++;
 	}
 	for (i = 0; i < DEFAULT_LEN;i++) {
 		int checker = 1,start = i;
-		while (text[i] != '\0') {		    
-			if (!isalpha(text[i])) {
-				checker = 0;
-			}
-			if (is_loud(text[i])) {
-				checker = 0;
-			}
-			i++;
+		while (text[i] != '\0') {			
+			if (is_loud(text[i]) || !isalpha(text[i])) checker = 0;
+			i++;		
 		}
 		if (checker && isalpha(text[start])) {			
 			printf("%i. ",++count);
